90927743b0e9ee9e5813c99e9c61b02169f60c7f
Naomi Greenberg <ngreenberg@luc.edu>
2023-09-27 12:40:55 -0500

    successful training on linear seperable and non linearly seperable data

--- perceptron.py
+++ perceptron.py
@@ -12,15 +12,16 @@ def predict(weights, X):
 def train(X, y, eta=0.01, n_iter=10):
     n_samples, n_features = X.shape
     weights = initialize_weights(n_features)
+    errors = []
 
     for _ in range(n_iter):
-        errors = 0
+        error = 0
         for xi, target in zip(X, y):
             update = eta * (target - predict(weights, xi)) 
             weights += update * xi
-            errors += int(update != 0.0)
-
-    return weights
+            error += int(update != 0.0)
+        errors.append(error)
+    return weights, errors
 
 '''linearly seperable data'''
 # Define features X1 and X2 for two classes
@@ -39,16 +40,14 @@ X = np.column_stack((X1, X2))
 y = np.array([1, 1, 1, 1, 1, -1, -1, -1, -1, -1])
 
 
-perceptron_weights = train(X, y, eta=0.01, n_iter=10)
+perceptron_weights, _ = train(X, y, eta=0.01, n_iter=10)
 
-plt.scatter(class1_X1, class1_X2, label='Class 1', marker='o', c='blue')
-plt.scatter(class2_X1, class2_X2, label='Class 2', marker='x', c='red')
 
-# Plot the decision boundary
 x_line = np.linspace(-4, 4, 100)
 y_line = (-perceptron_weights[1] / perceptron_weights[0]) * x_line
+plt.scatter(class1_X1, class1_X2, label='Class 1', marker='o', c='blue')
+plt.scatter(class2_X1, class2_X2, label='Class 2', marker='x', c='red')
 plt.plot(x_line, y_line, '-g', label='Decision Boundary')
-
 plt.xlabel('X1')
 plt.ylabel('X2')
 plt.legend(loc='best')
@@ -61,16 +60,16 @@ predictions = predict(perceptron_weights, X)
 
 # Calculate accuracy
 accuracy = np.mean(predictions == y) * 100
-print(f"Accuracy on the training set: {accuracy:.2f}%")
+print(f"Accuracy on the training set(Linearly Seperable): {accuracy:.2f}%")
 
 '''non linearly seperable data'''
 
 # Define features X1 and X2 for two classes
-class1_X1 = np.array([2.5, 1.5, 3.5, 3.0, 1.0])
-class1_X2 = np.array([3.0, 2.0, 4.0, 3.5, 1.5])
+class1_X1 = np.array([2.5, 1.5, 3.5, 3.0, 1.0, -1.0, -2.0, -2.5, -3.0, -3.5])
+class1_X2 = np.array([3.0, 2.0, 4.0, 3.5, 1.5, -1.5, -1.0, -2.0, -3.0, -2.5])
 
-class2_X1 = np.array([-2.0, -3.0, -2.5, -1.5, -1.0, 1.5, 0.5, 2.0, 2.5])
-class2_X2 = np.array([-2.5, -3.5, -2.0, -1.0, -1.5, 0.5, 1.5, 1.0, 3.0])
+class2_X1 = np.array([0.5, 0.0, -0.5, -1.0, -1.5, 1.5, 1.0, 2.0, 2.5, 3.0])
+class2_X2 = np.array([0.5, 0.0, -0.5, -1.0, -1.5, 1.5, 1.0, 2.0, 2.5, 3.0])
 
 # Create the dataset
 X1 = np.concatenate((class1_X1, class2_X1))
@@ -78,15 +77,22 @@ X2 = np.concatenate((class1_X2, class2_X2))
 X = np.column_stack((X1, X2))
 
 # Create the target labels (1 for class 1, -1 for class 2)
-y = np.array([1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
+y = np.array([1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
 
 # Train the perceptron
-perceptron_weights = train(X, y, eta=0.01, n_iter=100)
+perceptron_weights, error_count = train(X, y, eta=0.01, n_iter=20)
 
 # Plot the number of errors in each epoch
-plt.plot(range(1, len(perceptron_weights.errors_) + 1), perceptron_weights.errors_, marker='o')
+plt.plot(range(1, len(error_count) + 1), error_count, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of Errors')
 plt.title('Perceptron Convergence')
 plt.grid(True)
 plt.show()
+
+
+predictions = predict(perceptron_weights, X)
+
+# Calculate accuracy
+accuracy = np.mean(predictions == y) * 100
+print(f"Accuracy on the training set(Non-Linearly Seperable): {accuracy:.2f}%")
